1eb16cb2a8775937f83cfe75454f868d2c8eff3c
Joe Pea <peajoe@meta.com>
2024-11-05 17:44:57 -0800

    import three and build with esbuild with target es2020 to check for top-level await

---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import { WebGPURenderer } from "three/webgpu";
+
+// Uncomment the next line to cause a build error. When it is commented, there is no error because threejs does not have a top-level await.
+// await null;
+
+console.log(WebGPURenderer);
